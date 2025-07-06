b341a43b0f0d311b9eaa082e6b4e794fcc7ccd4c
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2025-07-06 23:50:34 +0600

    design complete

--- src/components/sections/works/works.tsx
+++ src/components/sections/works/works.tsx
@@ -1,51 +1,104 @@
-import React from 'react'
-import bedge from '../../../app/Badge.png'
-import CommonWrapper from '@/components/common/CommonWrapper'
-import left from '../../../app/Arrow-Left.png'
-import right from '../../../app/Arrow-Right.png'
-import active from '../../../app/active-project.png'
-import project1 from '../../../app/Project1.png'
-import project2 from '../../../app/Project2.png'
-import rightpic from '../../../app/right-pic.png'
-import leftpic from '../../../app/left-pic.png'
+import React from 'react';
+import Image from 'next/image';
+import bedge from '../../../app/Badge.png';
+import CommonWrapper from '@/components/common/CommonWrapper';
+import left from '../../../app/Arrow-Left.png';
+import right from '../../../app/Arrow-Right.png';
+import active from '../../../app/active-project.png';
+import project1 from '../../../app/Project1.png';
+import project2 from '../../../app/Project2.png';
+import rightpic from '../../../app/right-pic.png';
+import leftpic from '../../../app/left-pic.png';
 
 const Works = () => {
   return (
-    <div>
-    <CommonWrapper>
-        <div className='flex flex-col md:flex-row'>
-            <div>
-                <img src={bedge.src} alt="" />
-            </div>
-            <div>
-                <p className='pl-100 text-9xl font-bold bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text opacity-20'>Recent</p>
-                <p className='pl-150 text-9xl font-bold bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text opacity-20'>Works</p>
-            </div>
-            <div className='pl-5 pt-20'>
-                <p className='text-sm font-normal text-[#838A95]'>Ready to transform your hard work into <br /> a thriving, stress-free business?</p>
-                <div className='pt-5 flex flex-row'>
-                    <img src={left.src} alt="" />
-                    <img src={right.src} alt="" />
-                </div>
-            </div>
-        </div>
-    </CommonWrapper>
-    <div className='relative flex flex-row justify-between px-16'>
-        <div className='absolute -left-5 top-30'>
-            <img src={rightpic.src} alt="" />
-        </div>
-        <div className='flex flex-col md:flex-row'>
-            <img src={project1.src} alt="" className='absolute top-35 left-280' />
-            <img src={project2.src} alt="" className='absolute top-18 left-180'/>
-            <img src={active.src} alt="" className='absolute left-50' />
-            
-        </div>
-        <div className='absolute -right-5 top-30'>
-            <img src={leftpic.src} alt="" />
-        </div>
-    </div>
-    </div>
-  )
-}
+    <div className="relative overflow-hidden bg-white">
+      <CommonWrapper>
+        {/* Header Section */}
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-8">
+          {/* Badge */}
+          <div>
+            <Image src={bedge} alt="badge" width={40} height={40} />
+          </div>
 
-export default Works
+          {/* Title */}
+          <div className="text-center lg:text-left">
+            <h2 className="text-6xl lg:text-8xl font-bold bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text opacity-20">
+              Recent
+            </h2>
+            <h2 className="text-6xl lg:text-8xl font-bold bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text opacity-20">
+              Works
+            </h2>
+          </div>
+
+          {/* Description & Arrows */}
+          <div className="text-center lg:text-right max-w-xs">
+            <p className="text-sm text-[#838A95]">
+              Ready to transform your hard work into a thriving, stress-free
+              business?
+            </p>
+            <div className="flex justify-center lg:justify-end gap-4 mt-4">
+              <Image src={left} alt="Left Arrow" width={30} height={30} />
+              <Image src={right} alt="Right Arrow" width={30} height={30} />
+            </div>
+          </div>
+        </div>
+
+        {/* Projects */}
+      </CommonWrapper>
+      <div className="relative flex flex-col md:flex-row items-center justify-between overflow-hidden">
+        {/* Side Images for large screens */}
+        <div className="hidden lg:block">
+          <Image
+            src={rightpic}
+            alt="right decoration"
+            width={100}
+            height={100}
+          />
+        </div>
+
+        {/* Projects List */}
+        <div className="flex flex-wrap flex-row justify-center gap-6">
+          {/* Active Project */}
+          <div className="w-[320px] sm:w-[360px] rounded-xl overflow-hidden bg-white md:absolute md:h-[700px] md:top-0 md:left-45 md:">
+            <Image
+              src={active}
+              alt="active"
+              className="w-full h-auto object-cover"
+            />
+            {/* <div className="p-4">
+              <span className="text-xs px-2 py-1 bg-purple-100 text-purple-600 rounded">
+                2024, Business
+              </span>
+              <h3 className="text-lg font-semibold mt-2">
+                Business Consulting Service
+              </h3>
+            </div> */}
+          </div>
+          {/* Project 1 */}
+          <div className="w-[280px] sm:w-[320px] h-auto rounded-xl overflow-hidden bg-white md:absolute md:left-120 md:bottom-20 md:h-[500px]">
+            <Image
+              src={project2}
+              alt="project1"
+              className="w-full h-auto object-cover"
+            />
+          </div>
+
+          {/* Project 2 */}
+          <div className="w-[220px] sm:w-[280px] h-auto rounded-xl overflow-hidden bg-white relative">
+            <Image
+              src={project1}
+              alt="project2"
+              className="w-full h-auto object-cover"
+            />
+          </div>
+        </div>
+        <div className="hidden lg:block">
+          <Image src={leftpic} alt="left decoration" width={100} height={100} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
